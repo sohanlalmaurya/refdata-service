ebda934e6d0357df0028b88ffe4287e2657fc950
Sohan Maurya <sohmaury@publicisgroupe.net>
2019-12-06 10:50:17 +0530

    first commit

---
+++ src/main/java/com/sapient/refdata/service/RefdataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.refdata.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefdataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefdataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/refdata/service/RefdataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.refdata.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefdataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
