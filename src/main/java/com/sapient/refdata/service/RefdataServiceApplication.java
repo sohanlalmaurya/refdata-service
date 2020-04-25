5053d00df73bb6000a62e7f8c9ba8335e6cec465
sohan.maurya <sohanlal.maurya@gmail.com>
2020-04-25 23:26:28 +0530

    implement eureka naming server

--- src/main/java/com/sapient/refdata/service/RefdataServiceApplication.java
+++ src/main/java/com/sapient/refdata/service/RefdataServiceApplication.java
@@ -2,8 +2,10 @@ package com.sapient.refdata.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RefdataServiceApplication {
 
 	public static void main(String[] args) {
